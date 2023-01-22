3d38da7c2f1a7bf50e9ca7951298b1562a260fbc
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-01-21 20:01:24 -0600

    Cambio del pin D5 al B0 en el circuito. Agregación de más estados específicos al firmware para mejor la funcionalidad del juego

--- src/simon.c
+++ src/simon.c
@@ -3,57 +3,103 @@
 typedef enum {
 	IDLE,
 	INIT,
-	LED_ON,
-	LED_OFF,
+	INIT_LED_ON,
+	INIT_LED_OFF,
 	GAME_SETUP,
 	GAME_LED,
 	WAIT_USER,
 	VERIFY,
 	WIN,
-	LOSE
+	LOSE,
+	LOSE_LED_ON,
+	LOSE_LED_OFF
 } states;
 
 volatile states state = IDLE;
 volatile unsigned short init_led_done = 0;
+volatile unsigned short game_led_done = 0;
 volatile unsigned short game_led_count = 0;
-volatile unsigned short game_leds = 4;
+volatile unsigned short lose_led_done = 0;
+volatile unsigned short lose_led_count = 0;
+volatile unsigned short mem_leds = 4;
 volatile unsigned short button;
+
+ISR(INT0_vect) {
+	if (state == IDLE) {
+		state = INIT;
+	} else if (state == WAIT_USER) {
+		button = 1;
+		state = VERIFY;
+	}
+	
+}
+
+ISR(INT1_vect) {
+	if (state == IDLE) {
+		state = INIT;
+	} else if (state == WAIT_USER) {
+		button = 2;
+		state = VERIFY;
+	}
+}
+
+ISR(PCINT0_vect) {
+	if (state == IDLE) {
+		state = INIT;
+	} else if (state == WAIT_USER) {
+		button = 3;
+		state = VERIFY;
+	}
+}
 
 ISR(PCINT1_vect) {
 	if (state == IDLE) {
 		state = INIT;
-	}
-}
-
-ISR(PCINT2_vect) {
-	if (state == IDLE) {
-		state = INIT;
+	} else if (state == WAIT_USER) {
+		button = 0;
+		state = VERIFY;
 	}
 }
 
 ISR(TIMER0_OVF_vect) {
-	if (state == LED_ON) {
-		state = LED_OFF;
+	if (state == INIT_LED_ON) {
+		state = INIT_LED_OFF;
 		PORTB &= 0x00;
-	} else if (state == LED_OFF) {
+	} else if (state == INIT_LED_OFF) {
 		if (!init_led_done) {
-			state = LED_ON;
+			state = INIT_LED_ON;
 			PORTB |= 0xF0;
 			init_led_done = 1;
 		} else {
 			state = GAME_SETUP;
+		}
+	} else if (state == LOSE_LED_ON) {
+		state = LOSE_LED_OFF;
+		PORTB &= 0x00;
+	} else if (state == LOSE_LED_OFF) {
+		if (!lose_led_done) {
+			if (!lose_led_count) {
+				lose_led_count = 1;
+			} else {
+				lose_led_count = 2;
+				lose_led_done = 1;
+			}
+			state = LOSE_LED_ON;
+			PORTB |= 0xF0;
+		} else {
+			state = IDLE;
 		}
 	}
 }
 
 ISR(TIMER1_COMPA_vect) {
 	if (state == GAME_LED) {
-		if (game_led_count < (game_leds-1)) {
+		if (game_led_count < (mem_leds-1)) {
 			game_led_count++;
-		} else {
+		} else if (game_led_count == (mem_leds-1)) {
 			state = WAIT_USER;
-			PORTB = 0x00;
-			PORTD = 0x00;
+			game_led_done = 1;
+			game_led_count = 0;
 		}
 	}
 	
@@ -64,14 +110,15 @@ void lfsr16(unsigned short *rnd_number);
 int main(void) {
 	DDRA = 0x06;
 	PORTA = 0x00;
-	DDRB = 0xFF;
+	DDRB = 0xFE;
 	PORTB = 0x00;
-	DDRD = 0x63;
+	DDRD = 0x73;
 	PORTD = 0x00;
 
-	GIMSK = 0xD8;
+	MCUCR = 0x85;
+	GIMSK = 0xE8;
+	PCMSK = 0x01;
 	PCMSK1 = 0x01;
-	PCMSK2 = 0x10;
 	sei();
 
 	TCCR0A = 0x00;
@@ -92,6 +139,7 @@ int main(void) {
 	unsigned short game_leds[9];
 	unsigned short n = 0;
 	unsigned short led;
+	unsigned short delay;
 
 
 	while (1)
@@ -100,95 +148,99 @@ int main(void) {
 		case IDLE:
 			DDRA = 0x06;
 			PORTA = 0x00;
-			DDRB = 0xFF;
+			DDRB = 0xFE;
 			PORTB = 0x00;
 			DDRD = 0x63;
 			PORTD = 0x00;
+
+			n = 0;
+			init_led_done = 0;
+			game_led_done = 0;
+			game_led_count = 0;
+			lose_led_done = 0;
+			lose_led_count = 0;
+			mem_leds = 4;
 			break;
 		case INIT:
-			state = LED_ON;
+			state = INIT_LED_ON;
 			PORTB |= 0xF0;
 			TCNT0 = 0;
 			break;
-		case LED_ON:
+		case INIT_LED_ON:
 			break;
-		case LED_OFF:
+		case INIT_LED_OFF:
 			break;
 		case GAME_SETUP:
-			if (n_leds < game_leds) {
-				leds_position[n_leds] = rnd_number % game_leds;
-				n_leds++;
+			if (n < mem_leds) {
+				game_leds[n] = rnd_number % 4;
+				n++;
 			} else {
 				state = GAME_LED;
 				TCNT1H = 0;
 				TCNT1L = 0;
 			}
+			break;
 		case GAME_LED:
-			led = leds_position[game_led_count];
-			switch (led) {
-			case 0:
-				PORTB = 0x80;
-				PORTD = 0x00;
-				break;
-			case 1:
-				PORTB = 0x40;
-				PORTD = 0x00;
-				break;
-			case 2:
-				PORTB = 0x20;
-				PORTD = 0x00;
-				break;
-			case 3:
-				PORTB = 0x10;
-				PORTD = 0x00;
-				break;
-			case 4:
-				PORTB = 0x08;
-				PORTD = 0x00;
-				break;
-			case 5:
-				PORTB = 0x04;
-				PORTD = 0x00;
-				break;
-			case 6:
-				PORTB = 0x02;
-				PORTD = 0x00;
-				break;
-			case 7:
-				PORTB = 0x01;
-				PORTD = 0x00;
-				break;
-			case 8:
+			if (!game_led_done) {
+				led = game_leds[game_led_count];
+				switch (led) {
+				case 0:
+					PORTB = 0x80;
+					break;
+				case 1:
+					PORTB = 0x40;
+					break;
+				case 2:
+					PORTB = 0x20;
+					break;
+				case 3:
+					PORTB = 0x10;
+					break;
+				}
+			} else {
 				PORTB = 0x00;
-				PORTD = 0x40;
-				break;
-			default:
-				PORTB = 0x00;
-				PORTD = 0x00;
-				break;
+				game_led_count = 0;
 			}
+			break;
 		case WAIT_USER:
+			PORTB = 0x00;
 			break;
 		case VERIFY:
-			// led = leds_position[game_led_count];
-			// if (button == led) {
-			// 	if (game_led_count == (game_leds-1)) {
-			// 		state = WIN;
-			// 		game_led_count = 0;
-			// 	} else {
-			// 		state = WAIT_USER;
-			// 		game_led_count++;
-			// 	}
-			// } else {
-			// 	state = LOSE;
-			// 	game_led_count = 0;
-			// }
+			led = game_leds[game_led_count];
+			if (button == led) {
+				if (game_led_count == (mem_leds-1)) {
+					state = WIN;
+				} else {
+					state = WAIT_USER;
+					game_led_count++;
+				}
+			} else {
+				state = LOSE;
+			}
 			break;
 		case WIN:
-			PORTB = 0xF0;
+			n = 0;
+			game_led_done = 0;
+			game_led_count = 0;
+			lose_led_done = 0;
+			lose_led_count = 0;
+			if (mem_leds < 9) {
+				mem_leds++;
+				delay = (OCR1AH << 8) | (OCR1AL);
+				delay -= 195;
+				OCR1AL = delay;
+				OCR1AH = (delay >> 8);
+			}
+			state = GAME_SETUP;
 			break;
 		case LOSE:
-			PORTB = 0x0F;
+			state = LOSE_LED_ON;
+			PORTB |= 0xF0;
+			TCNT0 = 0;
+			break;
+		case LOSE_LED_ON:
+			break;
+		case LOSE_LED_OFF:
 			break;
 		default:
 			break;
